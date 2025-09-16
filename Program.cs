8caa9423ca602ce0e45eec22fd0bd62d04b2e6df
Daniel Aldemir <daniel.aldemir79@gmail.com>
2025-09-16 15:47:18 +0200

    Variabler och arrayer skapade, samt en metod fom listar tillgängliga filmer, tider och priser. Även en while loop skapad så man är kvar i programmet så länge man inte valt att avsluta

--- Program.cs
+++ Program.cs
@@ -5,30 +5,56 @@
         static void Main(string[] args)
         {
             int menuChoice = 0;
+            const string valuta = "SEK"; //Valuta
+            const double moms = 0.06; //6% moms
+            const double studentRabatt = 0.15; //15% rabatt för studenter
+            bool harStudentRabatt = false; //Håller reda på om kunden har studentrabatt eller inte
 
-            VisaMeny();
+            string[] filmer = { "Braveheart", "Gldiatorn", "Top Gun", "Avatar", "Inception" };
+            string[] tider = { "15:00", "18:00", "21:00" }; 
+            double[] priser = { 120, 100, 130, 140, 110 }; //Priserna motsvarar filmernas index i arrayen
 
-            switch (menuChoice)
+            
+            Console.WriteLine("Välkommen till BioBokaren!");
+
+            while (true)
             {
-
-                case 1:     //Lista filmer
+                if (menuChoice == 5) //Avsluta
+                {
+                    Console.WriteLine("Tack för att du använde BioBokaren. Välkommen åter!");
                     break;
+                }
 
-                case 2:     //Välj film & tid, ange biljetter
-                    break;
+                VisaMeny();
+                menuChoice = GetInt();
 
-                case 3:     //Lägg på/ta bort studentrabatt
-                    break;
+                switch (menuChoice)
+                {
 
-                case 4:     //Skriv ut kvitto
-                    break;
+                    case 1:     //Lista filmer
+                        Console.Clear();
+                        ListaFilmer(filmer, tider, priser, valuta);
 
-                case 5:     //Avsluta
-                    break;
+                        Console.WriteLine();
 
-                default:
-                    Console.WriteLine("Du har angett felaktigt val.");
-                    break;
+                        break;
+
+                    case 2:     //Välj film & tid, ange biljetter
+                        break;
+
+                    case 3:     //Lägg på/ta bort studentrabatt
+                        break;
+
+                    case 4:     //Skriv ut kvitto
+                        break;
+                   
+                    case 5:
+                        break;
+                    
+                    default:
+                        Console.WriteLine("Du har angett felaktigt val. Försök igen");
+                        break;
+                }
             }
 
         }
@@ -38,7 +64,6 @@
         static void VisaMeny()
         {
             Console.WriteLine();
-            Console.WriteLine("Välkommen till BioBokaren!");
             Console.WriteLine("1. Lista filmer");
             Console.WriteLine("2. Välj film & tid, ange biljetter");
             Console.WriteLine("3. Lägg på/ta bort studentrabatt");
@@ -48,6 +73,22 @@
         }
 
 
+        // Metod för att lista filmer och tider
+        static void ListaFilmer(string[] filmer, string[] tider, double[] priser, string valuta)
+        {
+            Console.WriteLine("Tillgängliga filmer:");
+            for (int i = 0; i < filmer.Length; i++)
+            {
+                Console.WriteLine($"{i + 1}. {filmer[i]}: Pris: {priser[i]} {valuta}");
+            }
+            Console.WriteLine();
+            Console.WriteLine("Tillgängliga tider:");
+            for (int j = 0; j < tider.Length; j++)
+            {
+                Console.WriteLine($"{j + 1}. {tider[j]}");
+            }
+        }
+        
         static int GetInt()
         {
             int helTal;
@@ -56,7 +97,9 @@
             {
                 Console.WriteLine("Du har inte angett ett heltal. Försök igen.");
             }
-        
+
+            return helTal;
+
         }
 
         static double GetDouble()
