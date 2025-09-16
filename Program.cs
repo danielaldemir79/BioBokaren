adde4d1719c93c464285c44fadb0c4a7c83ac1f4
Daniel Aldemir <daniel.aldemir79@gmail.com>
2025-09-16 15:18:23 +0200

    Skapat en metod för att visa meny samt förberett en switch sats för varje val.

--- Program.cs
+++ Program.cs
@@ -4,7 +4,48 @@
     {
         static void Main(string[] args)
         {
-            
+            int menuChoice = 0;
+
+            VisaMeny();
+
+            switch (menuChoice)
+            {
+                
+                case 1:     //Lista filmer
+                    break;
+
+                case 2:     //Välj film & tid, ange biljetter
+                    break;
+
+                case 3:     //Lägg på/ta bort studentrabatt
+                    break;
+
+                case 4:     //Skriv ut kvitto
+                    break;
+
+                case 5:     //Avsluta
+                    break;
+
+                default:
+                    Console.WriteLine("Du har angett felaktigt val.");
+                    break;
+            }
+
         }
+    
+    
+    
+        static void VisaMeny()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Välkommen till BioBokaren!");
+            Console.WriteLine("1. Lista filmer");
+            Console.WriteLine("2. Välj film & tid, ange biljetter");
+            Console.WriteLine("3. Lägg på/ta bort studentrabatt");
+            Console.WriteLine("4. Skriv ut kvitto");
+            Console.WriteLine("5. Avsluta");
+            Console.WriteLine();
+        }
+
     }
 }
